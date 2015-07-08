b3ad54638f8dafe180be24d9aa51e879ff2d32c1
Justin Sybrandt <jsybrand@dhcp19-19.nersc.gov>
2015-07-08 14:57:29 -0700

    fixed job scanner to handle big int

--- src/JobSplitter.java
+++ src/JobSplitter.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -10,7 +11,7 @@ import java.util.Scanner;
  */
 public class JobSplitter extends Thread{
 
-    private final double MAX_JOB_WEIGHT = 100;
+    private final double MAX_JOB_WEIGHT = 10000;
 
     private final double RM_FILE_WEIGHT=0.1;
     private final double CP_FILE_WEIGHT=1;
@@ -58,7 +59,7 @@ public class JobSplitter extends Thread{
     private void evalAndSplitJob(Job j) throws IOException
     {
         //there are certain jobs we WONT split
-        if(j.getType()== Job.Type.CREATE_DIR || j.getType()== Job.Type.RM_DIR)
+        if(j.getType()== Job.Type.CREATE_DIR || j.getType()== Job.Type.RM_DIR || j.getType()== Job.Type.MODIFY_FILES)
         {
             finalJobs.add(j);
             return;
@@ -83,15 +84,15 @@ public class JobSplitter extends Thread{
             }
 
             String file = in.next();
-            int size = in.nextInt();
+            BigInteger size = in.nextBigInteger();
 
             switch (j.getType()){
-                case CREATE_DIR:    currWeight+=size*CP_DIR_WEIGHT + CP_DIR_LINE_WEIGHT; break;
-                case RM_DIR:        currWeight+=size*RM_DIR_WEIGHT + RM_DIR_LINE_WEIGHT; break;
-                case CREATE_FILES:  currWeight+=size*CP_FILE_WEIGHT + CP_FILE_LINE_WEIGHT; break;
-                case RM_FILES:     currWeight+=size*RM_FILE_WEIGHT + RM_FILE_LINE_WEIGHT; break;
-                case MODIFY_FILES: currWeight+=size*SYNC_FILE_WEIGHT + SYNC_FILE_LINE_WEIGHT; break;
-                default:            currWeight+=size+1;
+                case CREATE_DIR:    currWeight+=size.doubleValue()*CP_DIR_WEIGHT + CP_DIR_LINE_WEIGHT; break;
+                case RM_DIR:        currWeight+=size.doubleValue()*RM_DIR_WEIGHT + RM_DIR_LINE_WEIGHT; break;
+                case CREATE_FILES:  currWeight+=size.doubleValue()*CP_FILE_WEIGHT + CP_FILE_LINE_WEIGHT; break;
+                case RM_FILES:     currWeight+=size.doubleValue()*RM_FILE_WEIGHT + RM_FILE_LINE_WEIGHT; break;
+                case MODIFY_FILES: currWeight+=size.doubleValue()*SYNC_FILE_WEIGHT + SYNC_FILE_LINE_WEIGHT; break;
+                default:            currWeight+=size.doubleValue()+1;
             }
 
             out.println(file + "\t" + size);
