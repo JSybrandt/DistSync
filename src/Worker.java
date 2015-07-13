ee474fd34421d87588d7e470550259c6a7d3303f
Justin Sybrandt <jsybrand@dhcp19-19.nersc.gov>
2015-07-13 13:25:55 -0700

    error finding I DIDNT START THEM

--- src/Worker.java
+++ src/Worker.java
@@ -142,6 +142,7 @@ public class Worker extends Thread {
                     cmd[2]=job.upToDateMountPoint+path;
                     cmd[3]=job.outOfDateMountPoint+path;
                     runners[i]= new SystemRunner(cmd,job.logFile);
+                    runners[i].start();
                     System.out.println("Runner#:"+i+" CP " + cmd[2] + "->" + cmd[3]);
                 }
             }
@@ -181,6 +182,7 @@ public class Worker extends Thread {
                     String path = scan.nextLine();
                     cmd[1]=job.outOfDateMountPoint+path;
                     runners[i]= new SystemRunner(cmd,job.logFile);
+                    runners[i].start();
                 }
             }
         }
@@ -236,6 +238,7 @@ public class Worker extends Thread {
                     cmd[2]=job.upToDateMountPoint+path;
                     cmd[3]=job.outOfDateMountPoint+path;
                     runners[i]= new SystemRunner(cmd,job.logFile);
+                    runners[i].start();
                 }
             }
         }
