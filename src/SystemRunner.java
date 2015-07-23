b9949c7a816e5c51e9e873697a49e40ee89c3c5c
Justin Sybrandt <jsybrand@dhcp19-19.nersc.gov>
2015-07-23 11:03:37 -0700

    verbal workers

--- src/SystemRunner.java
+++ src/SystemRunner.java
@@ -17,6 +17,12 @@ public class SystemRunner extends Thread {
     @Override
     public void run(){
         try {
+
+            String cmd="";
+            for(String s : command)cmd+=s;
+
+            System.out.println(cmd);
+
             Process proc = Runtime.getRuntime().exec(command);
             Scanner scan = new Scanner(proc.getErrorStream());
 
