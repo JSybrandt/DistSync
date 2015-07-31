a24ef8b6c405d7f4274cb2ef68aaf99096690aa6
root <root@tlfssv71.nersc.gov>
2015-07-31 15:49:19 -0700

    added readme, cleaned house.

--- scanAndGenJobsFS.sh
+++
@@ -1,6 +0,0 @@
-cd expirement/fresh/
-find -printf "%P\n" | ../../fakeScans/scanMaker.sh > ../../fakeScans/fresh.scan
-cd ../stale/
-find -printf "%P\n" | ../../fakeScans/scanMaker.sh > ../../fakeScans/stale.scan
-cd ../../jobSplitter/
-./compare -o ../fakeScans/stale.scan -n ../fakeScans/fresh.scan -f ../tmp/jobs/
